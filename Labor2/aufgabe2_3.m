43954e4a826d88ce13f2f257378042a1015a8107
dbarelop <dbarelop@gmail.com>
2016-04-11 17:03:04 +0200

    Implemented arithmetic encoding/decoding

--- Labor2/aufgabe2_3.m
+++ Labor2/aufgabe2_3.m
@@ -12,18 +12,19 @@ h = sum(prob) * i;
 sh_dict = shannonfanodict(symbols, prob);
 hf_dict = huffmandict(num2cell(symbols), prob);
 
-%arith_enc = arithenco(double(input), prob);
+seq = changem(double(input), 1:length(symbols), symbols);
+arith_enc = arithenco(seq, repetitions);
 shannon_enc = huffmanenco(double(input), sh_dict);
 huffman_enc = huffmanenco(double(input), hf_dict);
-%arith_dec = arithdeco(arith_enc, prob);
+arith_dec = arithdeco(arith_enc, repetitions, length(seq));
 shannon_dec = huffmandeco(shannon_enc, sh_dict);
 huffman_dec = huffmandeco(huffman_enc, hf_dict);
 
-%assert(strcmp(char(arith_dec)', input) == 1, 'Arithmetic coding output doesn''t match input');
+assert(isequal(seq, arith_dec), 'Arithmetic coding output doesn''t match input');
 assert(strcmp(char(shannon_dec)', input) == 1, 'Shannon-Fano coding output doesn''t match input');
 assert(strcmp(char(huffman_dec)', input) == 1, 'Huffman coding output doesn''t match input');
 
-%fprintf('Length in bits of the message ''%s'' using the arithmetic coding: %d\n', input, length(arith_enc));
+fprintf('Length in bits of the message ''%s'' using the arithmetic coding: %d\n', input, length(arith_enc));
 fprintf('Length in bits of the message ''%s'' using the Shannon-Fano coding: %d\n', input, length(shannon_enc));
 fprintf('Length in bits of the message ''%s'' using the Huffman coding: %d\n', input, length(huffman_enc));
 
